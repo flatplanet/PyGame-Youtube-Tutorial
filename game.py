327f61b4f3865e17753a826cebc05e3777f8f960
John Elder <johne4196@gmail.com>
2023-09-21 10:29:41 -0700

    Intro To Pygame!

---
+++ game.py
@@ -0,0 +1,52 @@
+import pygame
+
+# Pygame Setup Stuff
+pygame.init()
+screen = pygame.display.set_mode((800,500))
+pygame.display.set_caption('Codemy.com Pygame Tutorial')
+clock = pygame.time.Clock()
+running = True
+
+
+dt = 0
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+
+while running:
+	# poll for events
+	# pygame.QUIT event means that the user clicked the X to close the window
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	# Pick the screen color
+	screen.fill("silver")
+
+	# RENDER OUR GAME HERE
+	pygame.draw.circle(screen, "#033660", player_pos, 100)
+
+	# Move our circle
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_UP]:
+		running = False
+		player_pos.y -= 300 * dt
+	if keys[pygame.K_DOWN]:
+		player_pos.y += 300 * dt
+
+	if keys[pygame.K_LEFT]:
+		player_pos.x -= 300 * dt
+	if keys[pygame.K_RIGHT]:
+		player_pos.x += 300 * dt
+		
+	# flip the display to output our work to the screen
+	pygame.display.flip()
+
+
+	# Set the clock stuff / delta time in seconds since the last frame
+	# used for framerate independent physics
+	dt = clock.tick(60) / 1000
+
+
+
+
+pygame.quit()
